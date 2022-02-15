69c76b31381cf65bfa25cea92a36dcc5027bd80c
cat214 <catou0214@gmail.com>
2022-02-16 03:02:35 +0900

    ローディングアニメーション追加

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ console.log('mainjs start');
 
 var html = $("html");
 
-var loading_wrapper = ('#loading')
-var loading_logo = ('.loading__logo')
+var loadingWrapper = ('#loading')
+var loadingLogo = ('.loading__logo')
 var logo = $('.header-logo');
 var nav = $('.gnav');
 var hero_catchcopy = $('.hero-catchcopy');
@@ -17,47 +17,24 @@ var concept_image_wrapper = ('.concept-image__wrapper');
 
 var timeline = gsap.timeline();
 
-// timeline.fromTo([loading_wrapper, loading_logo],{
-//   duration: 2,
-//   opacity: 0,
-// },{
-//   duration: 2,
-//   opacity: 0,
-//   display: "none",
-// })
-// .from([logo, nav, hero_catchcopy, hero_image_wrapper], {
-//   duration: 2,
-//   opacity: 0,
-// })
-// .to(html, {
-//   duration: 2,
-//   overflow: "visible",
-//   delay: -2,
-// });
-
-gsap.registerPlugin(ScrollTrigger);
-
-// var concept_timeline = gsap.timeline();
-var tl = gsap.timeline();
-tl.from([concept_title, concept_catchcopy, concept_text],{
-  scrollTrigger: {
-    trigger: ".concept__inner",
-    toggleActions: "restart none none reset",
-    markers: true,
-  },
-    onEnter: () => console.log("えんたーーー"),
-    onLeave: () => console.log("りーーぶ"),
-    duration: 3,
-    opacity: 1,
-    y: 100,
-})
-.from(concept_image, {
+timeline.from([loadingWrapper, loadingLogo],{
   duration: 2,
-  scale: 1.5,
   opacity: 0,
-  ease: Power2.easeOut,
-  delay: -1,
 })
+.to([loadingWrapper, loadingLogo], {
+  duration: 2,
+  opacity: 0,
+  display: "none",
+})
+.from([logo, nav, hero_catchcopy, hero_image_wrapper], {
+  duration: 2,
+  opacity: 0,
+})
+.to(html, {
+  duration: 2,
+  overflow: "visible",
+  delay: -2,
+});
 
 
 console.log('mainjs end');
